d07943145c03fe3f27998430fcebef23f36138a4
j22075大谷祥輝 <j22075om@edu.tuis.ac.jp>
2025-06-11 14:32:46 +0900

    Update kadai6-1.py
    取得したデータのパラメータを追加しました。

--- kadai6-1.py
+++ kadai6-1.py
@@ -6,7 +6,12 @@ API_URL = "http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData"
 
 params = {
     "appId": APP_ID,
-    "statsDataId": "0003138226",  # 賞与データの統計表ID
+    "statsDataId": "0003138226",  # 夏季賞与（毎月勤労統計）
+    "cdArea": "00000",            # 全国
+    "cdCat01": "G",           # 情報通信業コード
+    "cdTime": "2013000000,2014000000,2015000000", #年次　2013,2014,2015年
+    "cdTab": "1101",             # 支給額
+    "cdEstablishment": "0,1,2,3",# 規模計, 30人未満, 30-99人, 100人以上
     "lang": "J",
 }
 
